19a1d52928feeac96bb516f0a63653827828cd13
Guilherme Eler <50060454+GuiEler@users.noreply.github.com>
2021-09-27 22:50:19 -0300

    refactor: http_adapter

--- lib/infra/http/http_adapter.dart
+++ lib/infra/http/http_adapter.dart
@@ -53,14 +53,18 @@ class HttpAdapter implements HttpClient {
     late Future<Response>? futureResponse;
 
     try {
-      if (method == 'post') {
-        futureResponse = client.post(Uri.parse(url),
-            headers: defaultHeaders, body: jsonBody);
-      } else if (method == 'get') {
-        futureResponse = client.get(Uri.parse(url), headers: defaultHeaders);
-      } else if (method == 'put') {
-        futureResponse =
-            client.put(Uri.parse(url), headers: defaultHeaders, body: jsonBody);
+      switch (method) {
+        case 'post':
+          futureResponse = client.post(Uri.parse(url),
+              headers: defaultHeaders, body: jsonBody);
+          break;
+        case 'get':
+          futureResponse = client.get(Uri.parse(url), headers: defaultHeaders);
+          break;
+        case 'put':
+          futureResponse = client.put(Uri.parse(url),
+              headers: defaultHeaders, body: jsonBody);
+          break;
       }
 
       if (futureResponse != null) {
@@ -74,22 +78,23 @@ class HttpAdapter implements HttpClient {
   }
 
   dynamic _handleResponse(Response response) {
-    if (response.statusCode == 200) {
-      return response.body.isEmpty ? {} : jsonDecode(response.body);
-    } else if (response.statusCode == 201) {
-      return response.body.isEmpty ? {} : jsonDecode(response.body);
-    } else if (response.statusCode == 204) {
-      return {};
-    } else if (response.statusCode == 400) {
-      throw HttpError.badRequest;
-    } else if (response.statusCode == 401) {
-      throw HttpError.unauthorized;
-    } else if (response.statusCode == 403) {
-      throw HttpError.forbidden;
-    } else if (response.statusCode == 404) {
-      throw HttpError.notFound;
-    } else {
-      throw HttpError.serverError;
+    switch (response.statusCode) {
+      case 200:
+        return response.body.isEmpty ? {} : jsonDecode(response.body);
+      case 201:
+        return response.body.isEmpty ? {} : jsonDecode(response.body);
+      case 204:
+        return {};
+      case 400:
+        throw HttpError.badRequest;
+      case 401:
+        throw HttpError.unauthorized;
+      case 403:
+        throw HttpError.forbidden;
+      case 404:
+        throw HttpError.notFound;
+      default:
+        throw HttpError.serverError;
     }
   }
 }
