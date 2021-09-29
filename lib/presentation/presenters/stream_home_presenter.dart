84bfdd06a5defdabee466f93523a497ebc088183
Guilherme Eler <50060454+GuiEler@users.noreply.github.com>
2021-09-28 21:31:04 -0300

    feat: configured stream home presenter

--- lib/presentation/presenters/stream_home_presenter.dart
+++ lib/presentation/presenters/stream_home_presenter.dart
@@ -1,3 +1,9 @@
+import 'dart:async';
+
+import 'package:flutter/foundation.dart';
+import '../../data/http/http.dart';
+import 'package:second_challenge/domain/usecases/load_coming_soon_data.dart';
+
 import '../../domain/entities/coming_soon.dart';
 import '../../domain/entities/keyword_data.dart';
 import '../../domain/usecases/load_keyword_data.dart';
@@ -5,26 +11,45 @@ import '../../ui/pages/home/home_presenter.dart';
 
 class StreamHomePresenter implements HomePresenter {
   final LoadKeywordData keywordDataLoader;
+  final LoadComingSoon comingSoonLoader;
 
   StreamHomePresenter({
     required this.keywordDataLoader,
+    required this.comingSoonLoader,
   });
+
+  final StreamController<List<ComingSoonEntity>> comingSoonStreamController =
+      StreamController<List<ComingSoonEntity>>();
+  final StreamController<List<KeywordDataEntity>> keywordDataStreamController =
+      StreamController<List<KeywordDataEntity>>();
 
   @override
   Stream<List<ComingSoonEntity>> get comingSoonStream =>
-      throw UnimplementedError();
+      comingSoonStreamController.stream;
 
   @override
   Stream<List<KeywordDataEntity>> get keywordDataStream =>
-      throw UnimplementedError();
+      keywordDataStreamController.stream;
 
   @override
-  Future<void> loadComingSoonData() {
-    throw UnimplementedError();
+  Future<void> loadComingSoonData() async {
+    try {
+      final List<ComingSoonEntity> comingSoon =
+          await comingSoonLoader.loadComingSoon();
+      comingSoonStreamController.add(comingSoon);
+    } on HttpError catch (error) {
+      debugPrint('$error');
+    }
   }
 
   @override
-  Future<void> loadKeywordData() {
-    throw UnimplementedError();
+  Future<void> loadKeywordData() async {
+    try {
+      final List<KeywordDataEntity> keywordData =
+          await keywordDataLoader.loadKeywordData();
+      keywordDataStreamController.add(keywordData);
+    } on HttpError catch (error) {
+      debugPrint('$error');
+    }
   }
 }
