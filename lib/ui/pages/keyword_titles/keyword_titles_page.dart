381aa775364752ec83ae5965d367e073255ffb50
Guilherme Eler <50060454+GuiEler@users.noreply.github.com>
2021-10-01 01:06:03 -0300

    fix: gridview crashing

--- lib/ui/pages/keyword_titles/keyword_titles_page.dart
+++ lib/ui/pages/keyword_titles/keyword_titles_page.dart
@@ -23,45 +23,67 @@ class _KeywordTitlesPageState extends State<KeywordTitlesPage> {
     final avaliableWidth = mediaQuery.width;
     return Scaffold(
       appBar: AppBar(),
-      body: SizedBox.expand(
-          child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16.0),
-        child: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SizedBox(
-                  width: avaliableWidth * 0.92,
-                  child: Text(
-                    capitalizeString(arguments.keyword),
-                    style: Theme.of(context).textTheme.headline4,
-                    overflow: TextOverflow.ellipsis,
-                    textAlign: TextAlign.center,
+      body: SafeArea(
+        minimum: const EdgeInsets.symmetric(vertical: 16.0),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8.0),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    width: avaliableWidth * 0.92,
+                    child: Text(
+                      capitalizeString(arguments.keyword),
+                      style: Theme.of(context).textTheme.headline4,
+                      overflow: TextOverflow.ellipsis,
+                      textAlign: TextAlign.center,
+                    ),
                   ),
-                ),
-              ],
-            ),
-            const SizedBox(
-              height: 16.0,
-            ),
-            Expanded(
-              child: GridView.count(
-                addAutomaticKeepAlives: false,
-                addRepaintBoundaries: false,
-                childAspectRatio: 110 / 160,
-                crossAxisCount: 3,
-                crossAxisSpacing: 16,
-                mainAxisSpacing: 20,
-                children: arguments.items!
-                    .map((item) => TitleCover(
-                        id: item.id, title: item.title, image: item.image))
-                    .toList(),
+                ],
               ),
-            ),
-          ],
+              const SizedBox(
+                height: 16.0,
+              ),
+              Expanded(
+                  child: GridView.builder(
+                cacheExtent: 9999,
+                physics: const BouncingScrollPhysics(),
+                itemCount: arguments.items!.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return TitleCover(
+                      id: arguments.items![index].id,
+                      title: arguments.items![index].title,
+                      image: arguments.items![index].image);
+                },
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  childAspectRatio: 110 / 160,
+                  crossAxisCount: 3,
+                  crossAxisSpacing: 16,
+                  mainAxisSpacing: 20,
+                ),
+                padding: const EdgeInsets.all(10),
+              )
+
+                  // child: GridView.count(
+                  //   addAutomaticKeepAlives: false,
+                  //   addRepaintBoundaries: false,
+                  //   childAspectRatio: 110 / 160,
+                  //   physics: const NeverScrollableScrollPhysics(),
+                  //   crossAxisCount: 3,
+                  //   crossAxisSpacing: 16,
+                  //   mainAxisSpacing: 20,
+                  //   children: arguments.items!
+                  //       .map((item) => TitleCover(
+                  //           id: item.id, title: item.title, image: item.image))
+                  //       .toList(),
+                  // ),
+                  ),
+            ],
+          ),
         ),
-      )),
+      ),
     );
   }
 }
